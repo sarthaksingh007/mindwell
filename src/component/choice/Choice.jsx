443e024cee99bac2798e095840907e7164542529
sarthaksingh007 <100351826+sarthaksingh007@users.noreply.github.com>
2023-10-30 13:53:31 +0530

    change in ui

--- src/component/choice/Choice.jsx
+++ src/component/choice/Choice.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from "react";
+import Box from '@mui/material/Box';
+import Modal from '@mui/material/Modal';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 import { Link } from "react-router-dom";
 import Delighted from "../../asset/delighted.png";
 import Good from "../../asset/good.png";
@@ -7,10 +11,26 @@ import NotGreat from "../../asset/notgreat.png";
 import Overwhelmed from "../../asset/overwhelmed.png";
 import "../../style/radio.css";
 
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
 
 
 const Choice = () => {
   
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
   const [formData, setFormData] = useState({
     mood: "",
     topic: "",
@@ -226,7 +246,27 @@ const Choice = () => {
         {/* <button type="submit" onClick={(e) => handleMoodTopic(e)}>
           Are you ready
         </button> */}
-        
+        <div>
+      <Button onClick={handleOpen}><button type="submit" onClick={(e) => handleMoodTopic(e)}>
+          Are you ready
+        </button></Button>
+      <Modal
+        keepMounted
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="keep-mounted-modal-title"
+        aria-describedby="keep-mounted-modal-description"
+      >
+        <Box sx={style}>
+          <Typography id="keep-mounted-modal-title" variant="h6" component="h2">
+            Text in a modal
+          </Typography>
+          <Typography id="keep-mounted-modal-description" sx={{ mt: 2 }}>
+            Duis mollis, est non commodo luctus, nisi erat porttitor ligula.
+          </Typography>
+        </Box>
+      </Modal>
+    </div>
       </div>
     </div>
   );
